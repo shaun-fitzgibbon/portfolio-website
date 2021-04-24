87149d2aef982b88203b6e165f29990fb786eeae
Shaun Fitzgibbon <5940631+Sfitzg@users.noreply.github.com>
2021-04-24 02:04:58 +0200

    style: cleaned up styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -27,7 +26,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
