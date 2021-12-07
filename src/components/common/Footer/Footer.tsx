de66d6352e1f7e681d2d755613ce27571940e1c7
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-07 22:33:28 +0200

    refactor: refactor footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,19 +1,57 @@
-import Image from 'next/image'
+import { Button, Container, Logo } from '@components/ui'
+import { FC } from 'react'
 import styles from './Footer.module.css'
 
-const Footer = () => {
+const Footer: FC = function () {
+  const date = new Date()
+  const year = date.getFullYear()
+
   return (
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
+    <footer className={styles.Footer} id="contact-section">
+      <Container>
+        <div className={styles.TopContent}>
+          <Logo />
+        </div>
+        <div className={styles.MiddleContent}>
+          <div>
+            <h2 className={styles.ColumnHeader}>Follow Me</h2>
+          </div>
+          <div>
+            <h2 className={styles.ColumnHeader}>Get in Contact</h2>
+            <form name="contact" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
+              <div>
+                <input type="text" placeholder="Name" name="name" />
+              </div>
+              <div>
+                <input type="email" placeholder="Email" name="email" />
+              </div>
+              <div>
+                <textarea
+                  placeholder="Description"
+                  name="description"
+                  id=""
+                  cols={30}
+                  rows={7}
+                ></textarea>
+              </div>
+              <div>
+                <Button type="submit">HIT ME UP</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Container>
+      <div className={styles.BottomWrapper}>
+        <Container>
+          <div className={styles.BottomContent}>
+            <div className={styles.Copywrite}>
+              {' '}
+              &copy; {year} - Shaun Fitzgibbon
+            </div>
+          </div>
+        </Container>
+      </div>
     </footer>
   )
 }
