fd3b2e7fcf8d66aac33e8553434e7b7cfccd2acf
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-11 14:31:18 +0200

    feat: add Scrolled animations and show and hide on scroll to navbar

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,26 +1,70 @@
-import { FC } from 'react'
+import { useState, useEffect, FC, useCallback } from 'react'
 import Link from 'next/link'
+import cn from 'classnames'
 
-import { Logo, Container, NavLinks } from '@components/ui'
+import { Logo, Container, NavMenu } from '@components/ui'
 import { MobileMenuToggle } from '@components/ui'
 import { useUI } from '@contexts'
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.scss'
 
 const Navbar: FC = () => {
-  const { displaySidebar, toggleSidebar } = useUI()
+  const { isSidebarOpen, toggleSidebar } = useUI()
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [scrolled, setScrolled] = useState(false)
+  const [visible, setVisible] = useState(true)
+
+  const handleScroll = useCallback(() => {
+    const currentScrollPos = window.scrollY
+
+    if (currentScrollPos > 0) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 600
+    )
+
+    setPrevScrollPos(currentScrollPos)
+
+    console.log(currentScrollPos)
+    console.log(prevScrollPos)
+  }, [prevScrollPos])
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [prevScrollPos, visible, handleScroll])
+
   return (
-    <div className={styles.navWrapper}>
+    <div
+      className={cn(
+        styles.Wrapper,
+        scrolled && styles.Scrolled,
+        visible && styles.visible
+      )}
+    >
       <Container>
-        <div className={styles.navContent}>
+        <div className={styles.Navbar}>
           <Link href="/">
             <a>
               <Logo />
             </a>
           </Link>
-          <div className={styles.navMenu}>
-            <NavLinks />
+          <div className={styles.DesktopOnly}>
+            <NavMenu />
           </div>
-          <MobileMenuToggle onClick={toggleSidebar} isActive={displaySidebar} />
+          <div className={styles.MobileOnly}>
+            <MobileMenuToggle
+              onClick={toggleSidebar}
+              isActive={isSidebarOpen}
+            />
+          </div>
         </div>
       </Container>
     </div>
