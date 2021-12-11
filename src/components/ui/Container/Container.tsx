8f0ac3dd1c7c8474c16b98b12f4f05fbd0cf5850
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-11 14:51:47 +0200

    style: capitalize SCSS declerations

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import styles from './Container.module.scss'
 
 const Container: FC = function ({ children }) {
-  return <div className={styles.container}>{children}</div>
+  return <div className={styles.Container}>{children}</div>
 }
 
 export default Container
