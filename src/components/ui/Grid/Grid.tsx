638b2d92d50d445c0fb22239503a814851762cd5
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-11 17:12:52 +0200

    refactor: convert Grid to SCSS

--- src/components/ui/Grid/Grid.tsx
+++ src/components/ui/Grid/Grid.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
-import styles from './Grid.module.css'
+import styles from './Grid.module.scss'
 
 const Grid: FC = function ({ children }) {
-  return <div className={styles.grid}>{children}</div>
+  return <div className={styles.Grid}>{children}</div>
 }
 
 export default Grid
