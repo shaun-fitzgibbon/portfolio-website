481f9dfc99002ce1b3cf273f5150deb17bb9c885
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-14 14:08:29 +0200

    refactor: sass to work with global imports for varibles and mixins

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   generateBuildId: () => 'build',
+  sassOptions: {
+    includePaths: ['./src'],
+    prependData: `@import "@assets/styles/main.scss";`,
+  },
 }
