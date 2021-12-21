66260b533868f09f402c8fb12cc5eab5ed9c05dc
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-21 22:00:03 +0200

    refactor: correct yarn to installs to work on linux architecture

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   generateBuildId: () => 'build',
   sassOptions: {
     includePaths: ['./src'],
-    prependData: `@import "@assets/styles/main.scss";`,
+    prependData: '@import "@assets/styles/main.scss";',
   },
 }
