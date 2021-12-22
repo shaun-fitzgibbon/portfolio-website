788deeda878226f396e62fff42ac441667525e21
Shaun Fitzgibbon <shaun@fitzindustries.com>
2021-12-22 19:19:06 +0200

    feat: add social icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Dribbble } from './Dribbble'
+export { default as Github } from './Github'
+export { default as Linkedin } from './Linkedin'
+export { default as Twitter } from './Twitter'
+export { default as Youtube } from './Youtube'
