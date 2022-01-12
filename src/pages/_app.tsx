584a5e54ac37c2c2e16a25e71dc596d3a747fd0b
Shaun Fitzgibbon <shaun@fitzindustries.com>
2022-01-12 14:38:34 +0200

    feat: add `ThemeProvider` to `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import type { AppProps } from 'next/app'
-import '@assets/styles/globals.scss'
-import { Head, Layout } from '@components/common'
-import { UIProvider } from '@contexts/UIContext/UIContext'
+import { ThemeProvider } from 'next-themes'
+
+import 'assets/styles/globals.scss'
+import { Head, Layout } from 'components'
+import { UIProvider } from 'contexts'
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head />
-      <UIProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </UIProvider>
+      <ThemeProvider defaultTheme="system">
+        <Head />
+        <UIProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UIProvider>
+      </ThemeProvider>
     </>
   )
 }
