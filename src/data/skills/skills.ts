fe29e04d98924bccd6a8bfe4d78498f05de4f046
Shaun Fitzgibbon <shaun@fitzindustries.com>
2022-01-12 14:31:47 +0200

    refactor: amend projects and Skills Data

---
+++ src/data/skills/skills.ts
@@ -0,0 +1,66 @@
+export interface Skill {
+  id: number
+  name: string
+  description?: string
+  logoUrl?: string
+  websiteUrl?: string
+}
+
+export const skills: Skill[] = [
+  {
+    id: 1,
+    name: 'SASS',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+  {
+    id: 2,
+    name: 'React',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+  {
+    id: 3,
+    name: 'Git',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+  {
+    id: 4,
+    name: 'NextJS',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+  {
+    id: 5,
+    name: 'GraphQL',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+  {
+    id: 6,
+    name: 'Javascript',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+  {
+    id: 7,
+    name: 'Netlify',
+    description:
+      'A tribute page designed and built for freecodecamp project. I chose to do this on George Lucas as I am a huge fan of the Star wars movies.',
+    logoUrl: '/images/projects/george-lucas.png',
+    websiteUrl: 'https://codepen.io/sfitzg/full/PopPeeb',
+  },
+]
